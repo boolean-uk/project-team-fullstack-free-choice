0b1e17bbf482ad2c41048f3b473247357863eb0e
hannah68 <reihaneh.naderi1989@gmail.com>
2022-03-09 12:17:25 +0000

    create getbooks

---
+++ src/server/controllers/book.js
@@ -0,0 +1,32 @@
+const { prisma } = require("../utils/prisma");
+const { SERVER_ERROR, SERVER_SUCCESS, PRISMA_ERROR } = require("../config.js");
+
+const getAllBooks = async( req, res ) => {
+    const posts = await prisma.book.findMany();
+    res.status(SERVER_SUCCESS.OK.CODE).json({data: posts});
+}
+
+const getBookById = async( req, res ) => {
+    const { id } = req.params;
+
+    const foundUser = await prisma.book.findUnique({
+        where: {
+            id
+        }
+    })
+
+    if(!foundUser){
+        res.status(SERVER_ERROR.NOT_FOUND.CODE).json({error: SERVER_ERROR.NOT_FOUND.MESSAGE})
+    }
+
+    res.status(SERVER_SUCCESS.OK.CODE).json({data: foundUser})
+}
+
+
+
+
+
+module.exports = {
+    getAllBooks,
+    getBookById
+}
--- src/server/routers/book.js
+++ src/server/routers/book.js
@@ -1,7 +1,12 @@
 const express = require('express');
 
 const {
-    
+    getAllBooks,
+    getBookById
 } = require('../controllers/book.js');
+
+router.get("/", getAllBooks);
+
+router.get("/:id", getBookById);
 
 module.exports = router;
