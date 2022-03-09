e8cd3e585e8d0d970cd119d619bede870e1b71ce
alexamaingard <maingard.alexa@gmail.com>
2022-03-09 18:06:41 +0100

    add MatchPage UI (missing Header and Footer)

---
+++ src/client/pages/matchPage/MatchPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+
+const MatchPage = () => {
+    return(
+        <>
+            <Header />
+            <div className='match-page-container'>
+            </div>
+            <Footer />
+        </>
+    )
+}
+
+export default MatchPage;
