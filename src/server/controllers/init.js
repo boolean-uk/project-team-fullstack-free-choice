59bba377b274f43fd2682f0d4637f0b65067bd4d
alexamaingard <maingard.alexa@gmail.com>
2022-03-10 14:54:27 +0100

    add functions to get book and clean raw data

---
+++ src/server/controllers/init.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+const getISBN = (industryIdentifiers) => {
+    let isbn;
+    
+    industryIdentifiers.map(indId => {
+        if(indId.type === 'ISBN_13'){
+            isbn = indId.identifier;
+        }
+    });
+
+    return isbn;
+}
+
+const cleanBookData = (rawData) => {
+    const isbn = getISBN(rawData.industryIdentifiers);
+
+    return {
+        title: rawData.title,
+        isbn: isbn,
+        description: rawData.description,
+        authors: rawData.authors
+    }
+}
+
+const getBookFromAPI = async (isbn) => {
+    const rawBookData = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`);
+    const book = cleanBookData(rawBookData.data.items[0].volumeInfo);
+    console.log(book);
+}
+
+getBookFromAPI('9781913322076');
