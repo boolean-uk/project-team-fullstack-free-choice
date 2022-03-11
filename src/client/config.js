33ffed632423e2fedc19dc3020a7a003ea5b4a38
hannah68 <reihaneh.naderi1989@gmail.com>
2022-03-11 14:44:30 +0000

    delete files

--- src/client/config.js
+++
@@ -1,9 +0,0 @@
-const USER_URL = {
-    USER_ROOT: 'http://localhost:4000/user/',
-    REGISTER: 'http://localhost:4000/user/register',
-    LOGIN: 'http://localhost:4000/user/login'
-};
-
-const LOCAL_STORAGE = {
-    TOKEN: 'token',
-};
