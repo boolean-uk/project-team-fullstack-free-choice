c6b2708cee0154cb7b3b855dcb8ad30bda6d70f8
Can-Tina <lennihart@gmail.com>
2022-03-11 10:45:49 +0000

    create header and add form functionality

--- src/client/pages/signIn/SignIn.jsx
+++ src/client/pages/signIn/SignIn.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { useState } from 'react';
 import '../../styles/signIn.css'
+import Header from '../../components/Header'
+
+const URL = process.env.REACT_APP_API_URL;
+const loginEndpoint = '/user/login'; 
+const loginURL = URL + loginEndpoint;
 
 const emptyUser = {
     username: '',
@@ -10,6 +15,20 @@ const emptyUser = {
 const SignIn = () => {
     const [loginDetails, setLoginDetails] = useState(emptyUser);
 
+    const postLogin = async (url, loginDetails) => {
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: localStorage.getItem(),
+            },
+            body: JSON.stringify(loginDetails)
+        })
+        const data = await res.json();
+        console.log('token:', data)
+        return data;
+    }
+
     const handleChange = e => {
         const { value, name } = e.target;
         setLoginDetails({
@@ -17,38 +36,50 @@ const SignIn = () => {
         });
     }
 
-    const handleSubmit = e => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
+        const token = await postLogin(loginURL, loginDetails);
+        console.log(token)
+        /*if (token) {
+            navigate('/home');
+        }*/
+
     }
 
     return (
-        <div className='signup-page'>
-            <div className='gap-one'></div>
-            <div className='signup-container'>
-                <form className='signup-form' onSubmit={e => handleSubmit(e)}>
-                    <label htmlFor="username">Username</label>
-                    <input
-                        type='text'
-                        name='username'
-                        id='username'
-                        value={loginDetails.username}
-                        onChange={e => handleChange(e)}
-                        required
-                    />
-                    <label htmlFor='password'>Password</label>
-                    <input
-                        type='password'
-                        name='password'
-                        id='password'
-                        value={loginDetails.password}
-                        onChange={e => handleChange(e)}
-                        required
-                    />
-                    <input type='submit' value='Go!' id='submit' />
-                </form>
+        <>
+            <Header />
+            <div className='signup-page'>
+                <div className='gap-one'></div>
+                <div className='signup-container'>
+                    <form className='signup-form' onSubmit={e => handleSubmit(e)}>
+                        <h1 className='signin-header'>Sign In</h1>
+                        <label htmlFor='username'>Username</label>
+                        <input
+                            type='text'
+                            name='username'
+                            id='username'
+                            value={loginDetails.username}
+                            onChange={e => handleChange(e)}
+                            required
+                        />
+                        <label htmlFor='password'>Password</label>
+                        <input
+                            type='password'
+                            name='password'
+                            id='password'
+                            value={loginDetails.password}
+                            onChange={e => handleChange(e)}
+                            required
+                        />
+                        <input type='submit' value='Go!' id='submit' />
+                        <p className='new-account'>New to Bookr? Sign up here!</p>
+                    </form>
+                </div>
+                <div className='gap-two'></div>
             </div>
-            <div className='gap-two'></div>
-        </div>
+        </>
+
     )
 }
 
--- src/client/pages/signUp/SignUp.jsx
+++ src/client/pages/signUp/SignUp.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { useState } from 'react';
 import '../../styles/signUp.css'
 
+import Header from '../../components/Header';
+
+const URL = process.env.REACT_APP_API_URL;
+const registerEndpoint = '/user/register';
+const registerURL = URL + registerEndpoint;
+
+
 const emptyUser = {
     email: '',
     username: '',
@@ -11,54 +18,74 @@ const emptyUser = {
 const SignUp = () => {
     const [userDetails, setUserDetails] = useState(emptyUser);
 
-    const handleChange = e => {
+    const postRegister = async (url, userDetails) => {
+        await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userDetails)
+        })
+        .then(res => res.json())
+        .then(res => {
+            localStorage.setItem('loginInfo', res.json);
+        })
+    }
+
+    const handleChange = (e) => {
         const { value, name } = e.target;
         setUserDetails({
             ...userDetails, [name]: value
         });
     }
 
-    const handleSubmit = e => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
+        postRegister(registerURL, userDetails); 
     }
 
     return (
-        <div className='signup-page'>
-            <div className='gap-one'></div>
-            <div className='signup-container'>
-                <form className='signup-form' onSubmit={e => handleSubmit(e)}>
-                    <label htmlFor='email'>Email</label>
-                    <input
-                        type='email'
-                        name='email'
-                        id='email'
-                        value={userDetails.email}
-                        onChange={e => handleChange(e)}
-                        required
-                    />
-                    <label htmlFor="username">Username</label>
-                    <input
-                        type='text'
-                        name='username'
-                        id='username'
-                        value={userDetails.username}
-                        onChange={e => handleChange(e)}
-                        required
-                    />
-                    <label htmlFor='password'>Password</label>
-                    <input
-                        type='password'
-                        name='password'
-                        id='password'
-                        value={userDetails.password}
-                        onChange={e => handleChange(e)}
-                        required
-                    />
-                    <input type='submit' value='Go!' id='submit' />
-                </form>
+        <>
+            <Header />
+            <div className='signup-page'>
+                <div className='gap-one'></div>
+                <div className='signup-container'>
+                    <form className='signup-form' onSubmit={e => handleSubmit(e)}>
+                        <h1 className='signup-header'>Sign Up</h1>
+                        <label htmlFor='email'>Email</label>
+                        <input
+                            type='email'
+                            name='email'
+                            id='email'
+                            value={userDetails.email}
+                            onChange={e => handleChange(e)}
+                            required
+                        />
+                        <label htmlFor='username'>Username</label>
+                        <input
+                            type='text'
+                            name='username'
+                            id='username'
+                            value={userDetails.username}
+                            onChange={e => handleChange(e)}
+                            required
+                        />
+                        <label htmlFor='password'>Password</label>
+                        <input
+                            type='password'
+                            name='password'
+                            id='password'
+                            value={userDetails.password}
+                            onChange={e => handleChange(e)}
+                            required
+                        />
+                        <input type='submit' value='Go!' id='submit' />
+                    </form>
+                </div>
+                <div className='gap-two'></div>
             </div>
-            <div className='gap-two'></div>
-        </div>
+        </>
+
     )
 }
 
