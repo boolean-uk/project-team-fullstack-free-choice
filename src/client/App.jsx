4ee282a3dd45938973e2aa0c0aaed2920f6b90bf
hannah68 <reihaneh.naderi1989@gmail.com>
2022-03-14 12:49:36 +0000

    create route pages

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
-// import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
-import LandingPage from './pages/landingPage/LandingPage';
+import Home from './pages/home/Home';
+import SignUp from './pages/signUp/SignUp';
+import SignIn from './pages/signIn/SignIn';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MatchPage from './pages/matchPage/MatchPage';
+import MyRecommendations from './pages/myRecommendations/MyRecommendations';
 
 function App() {
   return (
-    <div className="App">
-      <LandingPage />
-    </div>
+      <div className="App">
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/signup' element={<SignUp />}/>
+          <Route path='/signin' element={<SignIn />}/>
+          <Route path='/match' element={<MatchPage />}/>
+          <Route path='/recommendation' element={<MyRecommendations />}/>
+        </Routes>
+        <Footer/>
+      </div>
   );
 }
 
