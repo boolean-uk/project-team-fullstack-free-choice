36bdd607b1046b894ebbaeb3f7bd7f18807092d3
alexamaingard <maingard.alexa@gmail.com>
2022-03-15 12:38:38 +0100

    add mock data for book DB

---
+++ src/server/data.js
@@ -0,0 +1,175 @@
+const BOOKS_TO_SEED = [
+    {
+        isbn: '9780008376123',
+        group: 1,
+        tags: [
+            'Good vs. Evil', 
+            'Courage', 
+            'Heroism', 
+            'Myth', 
+            'Friendship', 
+            'Race', 
+            'Perseverance'
+        ]
+    },
+    {
+        isbn: '9780553588484',
+        group: 1,
+        tags: [
+            'Power', 
+            'Betrayal', 
+            'Family', 
+            'Society and Class', 
+            'Principles', 
+            'Coming of Age', 
+            'Justice and Judgment', 
+            'Memory and the Past',
+            'Strength and Skill',
+            'Gender'
+        ]
+    },
+    {
+        isbn: '9780003300093',
+        group: 1,
+        tags: [
+            'Good and Evil',
+            'Faith',
+            'Magic',
+            'Friendship',
+            'Nature and Growth',
+            'Creation',
+            'Temptation'
+        ]
+    },    
+    {
+        isbn: '9780375826689',
+        group: 1,
+        tags: [
+            'Language and Communication',
+            'Friendship',
+            'The Home',
+            'Strength and Skill',
+            'Identity',
+            'Fate and Free Will',
+            'Good vs. Evil',
+            'The Supernatural',
+            'Coming of Age',
+            'Exploration'
+        ]
+    },
+    {
+        isbn: '9781503261389',
+        group: 2,
+        tags: [
+            'The Consequences of Modernity',
+            'The Threat of Female Sexual Expression',
+            'The Promise of Christian Salvation',
+            'Madness',
+            'Fear of Outsiders',
+            'Money',
+            'Monstrosity',
+            'Vampires'
+        ]
+    },    
+    {
+        isbn: '9780751537284',
+        group: 2,
+        tags: [
+            'Good vs. Evil',
+            'History and Society',
+            'Folklore',
+            'Knowledge',
+            'Religion',
+            'Mentor and disciples'
+        ]
+    },    
+    {
+        isbn: '9781774378328',
+        group: 2,
+        tags: [
+            'Birth and Creation',
+            'Alienation',
+            'Monstrosity',
+            'Family',
+            'Dangerous Knowledge',
+            'Ambition',
+            'Revenge',
+            'Nature'
+        ]
+    },    
+    {
+        isbn: '9780141442464',
+        group: 2,
+        tags: [
+           'Monstrosity',
+           'Art',
+           'Youth and Beauty',
+           'The Superficiality of Society',
+           'The Dangers of Social Influence'
+        ]
+    },
+    {
+        isbn: '9780441172719',
+        group: 3,
+        tags: [
+            'Man and the Natural World',
+            'Fate and Free Will',
+            'Tradition and Customs',
+            'Coming of Age',
+            'Power',
+            'Religion',
+            'Politics',
+            'Fear',
+            'Family'
+        ]
+    },    
+    {
+        isbn: '9781250773012',
+        group: 3,
+        tags: [
+           'Games',
+           'Childhood and Growing Up',
+           'Compassion',
+           'Ruthlessness',
+           'Friends and Enemies',
+           'Humanity',
+           'Empathy',
+           'Destruction',
+           'Virtual Reality',
+           'Computers',
+           'Leadership',
+           'Morality and Survival'
+        ]
+    },    
+    {
+        isbn: '9781473619814',
+        group: 3,
+        tags: [
+            'Cloning',
+            'Chosen Family',
+            'Artificial Intelligence',
+            'Guerilla Warfare',
+            'Galaxies',
+            'Race',
+            'Exploration'
+        ]
+    },    
+    {
+        isbn: '9780553418026',
+        group: 3,
+        tags: [
+            'Isolation',
+            'Fear',
+            'Sacrifice',
+            'Perseverance',
+            'Science',
+            'Man Versus the Natural World',
+            'The Home',
+            'Friendship'
+        ]
+    }
+]
+
+module.exports = {
+    BOOKS_TO_SEED
+}
